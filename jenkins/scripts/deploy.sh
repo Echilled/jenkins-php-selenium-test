825cf582a381f40b0d029a1ca6e011def6bf9ac8
Echilled <echew140899@gmail.com>
2023-11-15 14:37:36 +0800

    Add files via upload

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
+
 set -x
+docker network rm test-network
 docker network create --subnet=172.18.0.0/16 test-network
 docker run -d -p 80:80 --name my-apache-php-app -v $(pwd)/src:/var/www/html --ip 172.18.0.2 php:7.2-apache
 sleep 1
@@ -7,3 +9,5 @@ set +x
 
 echo 'Now...'
 echo 'Visit http://localhost to see your PHP application in action.'
+
+
