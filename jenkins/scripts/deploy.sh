40f88b88e2a63514823ae34d17663ac8bcb07c25
Echilled <echew140899@gmail.com>
2023-11-15 14:21:49 +0800

    Update deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env sh
-
 set -x
-docker network rm test-network
 docker network create --subnet=172.18.0.0/16 test-network
 docker run -d -p 80:80 --name my-apache-php-app -v $(pwd)/src:/var/www/html --ip 172.18.0.2 php:7.2-apache
 sleep 1
@@ -9,4 +7,3 @@ set +x
 
 echo 'Now...'
 echo 'Visit http://localhost to see your PHP application in action.'
-
